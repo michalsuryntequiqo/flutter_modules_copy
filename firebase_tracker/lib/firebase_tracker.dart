babe585189bbb925479d1e384ada745cf4b61b99
Michał Surynt <michal.surynt@gmail.com>
2019-06-11 08:39:18 +0200

    Exported files

--- firebase_tracker/lib/firebase_tracker.dart
+++ firebase_tracker/lib/firebase_tracker.dart
@@ -1,7 +1,5 @@
 library firebase_tracker;
 
-/// A Calculator.
-class Calculator {
-  /// Returns [value] plus 1.
-  int addOne(int value) => value + 1;
-}
+export 'package:firebase_tracker/src/firebase_event_type.dart';
+export 'package:firebase_tracker/src/firebase_tracker.dart';
+
